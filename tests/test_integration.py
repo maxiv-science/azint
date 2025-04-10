349178e69d2cd28aba777496f9054ef228e88432
Meghdad Yazdi <meghdad.yazdi@maxiv.lu.se>
2025-04-10 13:29:51 +0000

    Modify test based on new the return of integrate()

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -4,22 +4,22 @@ from azint import AzimuthalIntegrator
 poni_file = 'tests/test.poni'
 img = np.ones((1000, 1000), dtype=np.uint16)
 
-def save_divide(a, b):
+def safe_divide(a, b):
     return np.divide(a, b, out=np.zeros_like(a), where=b!=0.0)
 
 def test1d():
     ai = AzimuthalIntegrator(poni_file, 4, 1000, solid_angle=False) 
-    res, _ = ai.integrate(img)
+    I, error_1d, _, _ = ai.integrate(img)
     assert(np.allclose(res, 1.0))
     
     ai = AzimuthalIntegrator(poni_file, 1, 1000, error_model='poisson', solid_angle=False) 
-    res, _ = ai.integrate(img)
+    I, error_1d, _, _ = ai.integrate(img)
     assert(np.allclose(res, 1.0))
     
 def test2d():
     ai = AzimuthalIntegrator(poni_file, 4, 1000, 360, solid_angle=False) 
-    res, _, norm = ai.integrate(img)
-    projection = save_divide(np.sum(res, axis=0), np.sum(norm, axis=0))
+    I, error_1d, I_2d, error_2d = ai.integrate(img)
+    projection = safe_divide(np.sum(I_2d, axis=0), np.sum(ai.norm_2d, axis=0))
     assert(np.allclose(projection, 1.0))
     
 def test_custom_ranges():
